905bfbf79c042699abf17ad30bd0a29a09b57e78
Joel <jojabeet@gmail.com>
2023-12-13 21:31:54 -0500

    beach added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="animated-title">UWI Christmas Pixie</h1>
-      {/* More content will go here */}
+      <div style={{ zIndex: 2 }}>
+        Ackee and Morou Christmas Pixie
+      </div>
+      <video autoPlay loop muted style={{
+        position: "absolute",
+        width: "100%",
+        left: "50%",
+        top: "50%",
+        height: "100%",
+        objectFit: "cover",
+        transform: "translate(-50%, -50%)",
+        zIndex: "-1"
+      }}>
+        <source src="/beach.mp4" type="video/mp4"/>
+      </video>
     </div>
   );
 }
