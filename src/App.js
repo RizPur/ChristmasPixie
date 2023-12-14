77ea548abef17945b1c30975a09db693357d7be5
villa <d.thomas71@yahoo.com>
2023-12-14 00:38:46 -0500

    added the counter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { useEffect, useRef } from 'react';
 import lottie from 'lottie-web';
 import './App.css';
 import Button from '@mui/material/Button';
+import Countdown from './components/counter';
 
 const App = () => {
-  const treeRef= useRef(null);
-  const fireRef= useRef(null)
+  const treeRef = useRef(null);
+  const fireRef = useRef(null)
 
   useEffect(() => {
     // Lad othe Lottie christmas-tree 
     const treeAnim = lottie.loadAnimation({
-      container: treeRef.current, 
+      container: treeRef.current,
       renderer: 'svg',
       loop: true,
       autoplay: true,
-      path: '/christmas-tree.json' 
+      path: '/christmas-tree.json'
     });
 
     const fireAnim = lottie.loadAnimation({
@@ -24,28 +25,29 @@ const App = () => {
       autoplay: true,
       path: '/fire.json'
     });
-  
-  
+
+
     return () => {
       treeAnim.destroy();
       fireAnim.destroy();
-    } 
+    }
   }, []);
-  
+
   return (
     <div className="App">
+      <Countdown targetDate={new Date('December 22, 2023 23:59:59')} />
       <h1 style={{ fontSize: '4em', fontWeight: 'bold', marginBottom: '20px' }}>A&M Christmas Pixie</h1>
       <Button variant="contained" color="primary" style={{ fontSize: '1.5em' }}>
         Join
       </Button>
       <div ref={treeRef} style={{ width: 400, height: 400, margin: '0 auto' }}></div>
-      <div ref={fireRef} style={{ 
-        width: 200, 
-        height: 200, 
-        position: 'absolute', 
-        bottom: '30px', 
-        right: '30px' 
-    }}></div>
+      <div ref={fireRef} style={{
+        width: 200,
+        height: 200,
+        position: 'absolute',
+        bottom: '30px',
+        right: '30px'
+      }}></div>
       <video autoPlay loop muted style={{
         position: "absolute",
         width: "100%",
@@ -56,7 +58,7 @@ const App = () => {
         transform: "translate(-50%, -50%)",
         zIndex: "-1"
       }}>
-        <source src="/beach.mp4" type="video/mp4"/>
+        <source src="/beach.mp4" type="video/mp4" />
       </video>
     </div>
   );
---
+++ src/components/counter.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+
+const Countdown = ({ targetDate }) => {
+    const calculateTimeLeft = () => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        // <div>
+        //     {timeLeft.days ? <h2><span>{timeLeft.days} days </span></h2> : null}
+        //     {timeLeft.hours ? <h2><span>{timeLeft.hours} hours </span></h2> : null}
+        //     {timeLeft.minutes ? <h2><span>{timeLeft.minutes} minutes </span></h2> : null}
+        //     {timeLeft.seconds ? <h2><span>{timeLeft.seconds} seconds </span></h2> : null}
+        //     {/* {timeLeft.days ? <h2>{timeLeft.days} days</h2> : null}
+        //     {timeLeft.hours ? <h2>{timeLeft.hours} hours </h2> : null}
+        //     {timeLeft.minutes ? <h2><span>{timeLeft.minutes} minutes </span></h2> : null}
+        //     {timeLeft.seconds ? <h2><span>{timeLeft.seconds} seconds </span></h2> : null} */}
+
+        //     {Object.keys(timeLeft).length === 0 && <span>Countdown expired!</span>}
+        // </div>
+        <div style={styles.container}>
+            {timeLeft.days ? (
+                <h2 style={styles.text}>{timeLeft.days} days </h2>
+            ) : null}
+            {timeLeft.hours ? (
+                <h2 style={styles.text}>{timeLeft.hours} hours </h2>
+            ) : null}
+            {timeLeft.minutes ? (
+                <h2 style={styles.text}>{timeLeft.minutes} minutes </h2>
+            ) : null}
+            {timeLeft.seconds ? (
+                <h2 style={styles.text}>{timeLeft.seconds} seconds </h2>
+            ) : null}
+
+            {Object.keys(timeLeft).length === 0 && (
+                <h2 style={styles.text}>Countdown expired!</h2>
+            )}
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    text: {
+        margin: '0 10px',
+        color: '#333',
+        fontFamily: 'Arial, sans-serif',
+        fontSize: '24px',
+    },
+};
+
+export default Countdown;
