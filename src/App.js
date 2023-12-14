ec6a32f12c178523cc20efe312dc01999f5aefe9
Joel <jojabeet@gmail.com>
2023-12-13 22:04:58 -0500

    Christmas Tree added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 import './App.css';
 import Button from '@mui/material/Button';
 
 function App() {
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    // Lad othe Lottie christmas-tree 
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current, 
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '/christmas-tree.json' 
+    });
+  
+    return () => anim.destroy(); 
+  }, []);
+  
+
   return (
     <div className="App">
-      <h1 style={{ fontSize: '6em', fontWeight: 'bold', marginBottom: '20px' }}>Christmas Pixie</h1>
-      <Button variant="contained" color="primary" style={{ fontSize: '2.5em' }}>
+      <h1 style={{ fontSize: '4em', fontWeight: 'bold', marginBottom: '20px' }}>A&M Christmas Pixie</h1>
+      <Button variant="contained" color="primary" style={{ fontSize: '1.5em' }}>
         Join
       </Button>
+      <div ref={animationContainer} style={{ width: 400, height: 400, margin: '0 auto' }}></div>
       <video autoPlay loop muted style={{
         position: "absolute",
         width: "100%",
